1a0f4ed2dd39f710363c234bb4210b40ced599b4
andyding <andy.ding89@gmail.com>
2016-01-27 23:24:20 +0000

    commented out the console.logs

--- answers.js
+++ answers.js
@@ -4,13 +4,13 @@
 var firstChar = function(someString){
     return someString[0];
 };
-console.log(firstChar("Andy"));
+//console.log(firstChar("Andy"));
 
 //Function that takes a string and returns the last character of a string. 
 var lastChar = function(someString){
     return someString[someString.length-1];  
 };
-console.log(lastChar("Andy"));
+//console.log(lastChar("Andy"));
 
 //Write a function that takes a string and a number, and returns the character at the position represented by the number
 var inputChar = function(someString,someNumber){
@@ -21,19 +21,19 @@ var inputChar = function(someString,someNumber){
         return someString[someNumber]; 
     }
 };
-console.log(inputChar("Andy",2));
+//console.log(inputChar("Andy",2));
 
 //Write a function that takes two numbers and adds them together.
 var addNumbers = function(num1,num2){
     return num1+num2;
 };
-console.log(addNumbers(25,26));
+//console.log(addNumbers(25,26));
 
 //Write a function that takes two numbers and multiplies them together
 var multiplyNumbers = function(num1,num2){
     return num1*num2;
 };
-console.log(multiplyNumbers(4,3));
+//console.log(multiplyNumbers(4,3));
 
 //Write a function that takes two numbers and a string. 
 //If the string is 'add', then return the sum of the numbers
@@ -55,7 +55,7 @@ var operationNumbers = function(operation,num1,num2){
             return 0;
     }
 };
-console.log(operationNumbers('div',49,7));
+//console.log(operationNumbers('div',49,7));
 
 //Write a function that takes a string and number, and returns the string repeated that many number of times.
 var repeatString = function(someString,numRepeat){
@@ -65,7 +65,7 @@ var repeatString = function(someString,numRepeat){
     }
     return aString;
 };
-console.log(repeatString('hi',5));
+//console.log(repeatString('hi',5));
 
 //Write a function that takes a string, and returns the reverse of that string.
 var reverseString = function(someString){
@@ -75,7 +75,7 @@ var reverseString = function(someString){
     }
     return aString;
 };
-console.log(reverseString('supercalifragilisticexpialidocious'));
+//console.log(reverseString('supercalifragilisticexpialidocious'));
 
 //Write a function that takes a number and returns the factorial of a number
 var factorialNum = function(someNumber){
@@ -96,7 +96,7 @@ var factorialNum = function(someNumber){
         return aNumber;
     }
 };
-console.log(factorialNum(-5));
+//console.log(factorialNum(-5));
 
 //Write a function that takes a phrase as a string, and returns the longest word in that phrase.
 var longestWord = function(phrase){
@@ -109,7 +109,7 @@ var longestWord = function(phrase){
     }
     return longWord;
 };
-console.log(longestWord('This is a random sentence with wordssss'));
+//console.log(longestWord('This is a random sentence with wordssss'));
 
 //Write a function that takes a phrase, and returns the same phrase with every word capitalized
 var capitalFirst = function(phrase){
@@ -123,7 +123,7 @@ var capitalFirst = function(phrase){
     }
     return reconstruct;
 };
-console.log(capitalFirst("hI thIs is aNDY"));
+//console.log(capitalFirst("hI thIs is aNDY"));
 
 //Write a function that takes an array and returns the largest number of the array
 var largestNumber = function(array){
@@ -135,7 +135,7 @@ var largestNumber = function(array){
     }
     return bigNumber;
 };
-console.log(largestNumber([100,20000,3,4,9000,900]));
+//console.log(largestNumber([100,20000,3,4,9000,900]));
 
 //Write a function that takes an array, and returns a filtered array. The filtered array should contain only the truthy values from the initial array.
 var truthyFilter = function(array){
@@ -149,7 +149,7 @@ function filterByTruthyness(value) {
         return false;
     }
 }
-console.log(truthyFilter([4,0,true,false,"","hello"]));
+//console.log(truthyFilter([4,0,true,false,"","hello"]));
 
 //Write a function that takes an array of numbers, and returns the sum of all the numbers in the array.
 var sumArray = function(array){
@@ -159,7 +159,7 @@ var sumArray = function(array){
     }
     return sum;
 };
-console.log(sumArray([50,1,2,3,4]));
+//console.log(sumArray([50,1,2,3,4]));
 
 //Write a function that takes two arrays, and returns an array of all elements that are only in one array.
 var uniqueArray = function(array1,array2){
@@ -177,7 +177,7 @@ var uniqueArray = function(array1,array2){
     }
     return finalArray;
 };
-console.log(uniqueArray([1,2,3,60,4,5],[3,4,5,6]));
+//console.log(uniqueArray([1,2,3,60,4,5],[3,4,5,6]));
 
 //Mini Challenge: write a function that takes an array and a function as arguments.
 var mapArray = function(array,aFunction){
@@ -190,4 +190,4 @@ var mapArray = function(array,aFunction){
 var addOne = function(num){
     return num+1;
 };
-console.log(mapArray([1,5,10],addOne));
+//console.log(mapArray([1,5,10],addOne));
