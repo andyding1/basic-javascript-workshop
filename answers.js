55a68f51cddff42fb97752bd815324feb8135cdb
andyding <andy.ding89@gmail.com>
2016-01-27 20:52:53 +0000

    Returns last character of string

--- answers.js
+++ answers.js
@@ -5,3 +5,9 @@ var firstChar = function(someString){
     return someString[0];
 };
 console.log(firstChar("Andy"));
+
+//Function that takes a string and returns the last character of a string. 
+var lastChar = function(someString){
+    return someString[someString.length-1];  
+};
+console.log(lastChar("Andy"));
