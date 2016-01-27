eb626c1df5594a86c449f55104980737ff3dd4d7
andyding <andy.ding89@gmail.com>
2016-01-27 20:51:49 +0000

    First character of string exercise

--- answers.js
+++ answers.js
@@ -1,1 +1,7 @@
 //This is my answers.js file
+
+//Function that takes a string and returns the first character of the string.
+var firstChar = function(someString){
+    return someString[0];
+};
+console.log(firstChar("Andy"));
