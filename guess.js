5c3723676fdbf6acbb660cf30c9192cf802c5670
andyding <andy.ding89@gmail.com>
2016-01-28 01:16:33 +0000

    Added the wrong guesses if you did not guess the right number

--- guess.js
+++ guess.js
@@ -3,9 +3,11 @@ function randomNumber() {
     return number;
 }
 var number = randomNumber();
+var answerArray =[];
 var numberGame = function(){
     var win = false;
     var userInput = parseInt(prompt("Guess a number"));
+    answerArray = [userInput];
     for(var i=0;i<3;i++){
         if (userInput===number){
             win = true;
@@ -14,10 +16,12 @@ var numberGame = function(){
         else if(userInput<number){
             alert("Number is higher");
             userInput = parseInt(prompt("Guess another number"));
+            answerArray.push(userInput);
         }
         else if(userInput>number){
             alert("Number is lower");
             userInput = parseInt(prompt("Guess another number"));
+            answerArray.push(userInput);
         }
     }
     return win;
@@ -27,5 +31,5 @@ if (numberGame()){
     alert("You have won!");
 }
 else{
-    alert("Please try playing again, the number was: " + number);
+    alert("Please try playing again, the number was: " + number +"\n" + "These were your guesses: " + answerArray);
 }
